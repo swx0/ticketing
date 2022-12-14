b1c9d0d30505133d9058f419aa24a8b1b9d282a9
See Wei Xun <76123658+swx0@users.noreply.github.com>
2022-12-14 21:44:42 +0800

    added tests for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.use(json());
-
-// Trust traffic that is proxyed to application through ingress nginx is secured
-app.set('trust proxy', true);
-
-// disable encryption for cookie as JWT already encrypted
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// express captures the error and sends to middleware
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   // check environment var is found at startup
