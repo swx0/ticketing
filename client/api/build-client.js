f1781dff7660624538094e896517c25f8f6a8126
See Wei Xun <76123658+swx0@users.noreply.github.com>
2022-12-16 05:13:21 +0800

    added getInitialProps for displaying user signed in on landing page

---
+++ client/api/build-client.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const buildClient = ({ req }) => {
+  // window only exist in browser, does not exist in nodejs env
+  if (typeof window === 'undefined') {
+    // In server
+    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+
+    // Domain is not specified for server, ingress wont know which set of rules to use (need to forward headers)
+
+    //preconfigured axios
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    // In browser
+    // requests can be made with base url of ''
+    return axios.create({ baseURL: '/' });
+  }
+};
+
+export default buildClient;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const landing = () => {
+import buildClient from '../api/build-client';
+import axios from 'axios';
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 };
 
-export default landing;
+// will be called while rendering app on server
+// used to fetch data that this component needs during SSR
+// data loading cannot occur in component, as components only rendered once in SSR
+LandingPage.getInitialProps = async (context) => {
+  // window only exist in browser, does not exist in nodejs env
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
