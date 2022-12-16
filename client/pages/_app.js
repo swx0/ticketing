7d18ff021e9d3dc5b2b360a85d96c02c0e612b73
See Wei Xun <76123658+swx0@users.noreply.github.com>
2022-12-17 01:12:52 +0800

    completed auth

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 // This file is for global css that needs to appear in every page
 // will always load up for users to this app
-const wrapper = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default wrapper;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('api/users/currentuser');
+
+  // This is invoking the getInitialProps of the LandingPage (index.js) or signup.js etc
+  let pageProps = {};
+  // For the case where getInitialProps defined
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, currentUser: data.currentUser };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,7 @@
 import buildClient from '../api/build-client';
 import axios from 'axios';
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing page</h1>;
+  return currentUser ? <h1>You are signed in</h1> : <h1>Not signed in</h1>;
 };
 
 // will be called while rendering app on server
