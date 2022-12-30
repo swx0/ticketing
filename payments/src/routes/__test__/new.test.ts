55b88d32cc9475ff4bdd6698bcfb3d89aef7bc9d
See Wei Xun <76123658+swx0@users.noreply.github.com>
2022-12-31 00:44:09 +0800

    remove stripe test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -60,29 +60,3 @@ it('returns 400 when purchasing a cancelled order', async () => {
     })
     .expect(400);
 });
-
-it('returns a 201 with valid inputs', async () => {
-  const userId = new mongoose.Types.ObjectId().toHexString();
-  const order = Order.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    version: 0,
-    userId,
-    price: 100,
-    status: OrderStatus.Created,
-  });
-  await order.save();
-
-  await request(app)
-    .post('/api/payments')
-    .set('Cookie', global.signup(userId))
-    .send({
-      token: 'tok_visa',
-      orderId: order.id,
-    })
-    .expect(201);
-
-  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
-  expect(chargeOptions.source).toEqual('tok_visa');
-  expect(chargeOptions.amount).toEqual(20 * 100);
-  expect(chargeOptions.currency).toEqual('usd');
-});
